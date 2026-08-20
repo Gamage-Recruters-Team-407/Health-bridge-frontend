1ca090f963bbbcc460b1337dd89620a5208ced97
mohamed nasfan <mohamednashfan01@gmail.com>
2026-08-20 23:21:15 +0530

    feat: upload frontend integration and UI components

---
+++ src/components/loaders/index.ts
@@ -0,0 +1,8 @@
+export {
+  FullScreenLoader,
+  SectionLoader,
+  DashboardSkeletonLoader,
+  default as FullScreenLoaderDefault,
+} from "./LoadingScreen";
+export type { LoadingScreenProps } from "./LoadingScreen";
+export { Spinner, PageLoader, Skeleton } from "../ui/Loader";
